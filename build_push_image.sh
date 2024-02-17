e7ed2ceac8a5436b5ab99fc8e5810fad8d8e06cf
Robert Theo <robertheo.rt@gmail.com>
2024-02-17 23:37:39 +0700

    Initial setup

---
+++ build_push_image.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+
+#Create a Docker image from a Dockerfile
+docker build . -t ghcr.io/robertheo15/order-service:v1
+#View a list of local Docker images
+docker images
+#Changing the image name according to the Docker Hub format
+docker tag order-service:v1 ghcr.io/robertheo15/order-service:v1
+#Login to Docker Hub
+cat ./my_password.txt | docker login --username robertheo --password-stdin
+#Upload image to Docker Hub
+docker push ghcr.io/robertheo15/order-service:v1
